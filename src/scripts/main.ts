661bfc510f5e2eb9a559ad6fe580d7397ee6d998
AKot1988 <kulibabenko@ukr.net>
2024-02-27 16:20:58 +0200

    add tabComponent via createElement

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,1 +1,8 @@
 import "../scss/main.scss";
+import CarCardComponent from "../scripts/copmponent/CarCardComponent/CarCardComponent";
+
+import { carCardData, tabsData } from "../../dataJSON/carDada.ts";
+
+
+
+carCardData.forEach((car) => {new CarCardComponent(car, document.querySelector('.popular__cars'))})
