8f73835df8c76925902a18b7037507bda8b45aad
AKot1988 <kulibabenko@ukr.net>
2024-02-27 16:23:35 +0200

    change path of creteElement

--- src/scripts/copmponent/CreateElement/CreateElement.ts
+++ src/scripts/copmponent/CreateElement/CreateElement.ts
@@ -1,76 +1,42 @@
-export default class CreateElement<Props> {
+export default class CreateElement {
     public tagName: string;
-    public params: Props
-    constructor(tagName: string, params: Props) {
+    public params: {
+        [key: string]: unknown
+    }
+    constructor(
+        tagName: string,
+        params: {
+            [key: string]: unknown
+        }
+    ) {
         this.tagName = tagName
         this.params = params
     }
-
     render(): HTMLElement {
-        const element = document.createElement(this.tagName)
+        const element = document.createElement(this.tagName) as HTMLElement
         for (let key in this.params) {
             switch (key) {
                 case 'dataset':
-                    for (let dataKey in this.params[key]) {
-                        element.dataset[dataKey] = this.params[key][dataKey]
+                    if (typeof this.params[key] === 'object') {
+                        for (let dataKey in this.params[key] as object) {
+                            element.dataset[dataKey] = (this.params[key] as keyof object)[dataKey]
+                        } 
+                    }   
+                    break
+                case 'classes': 
+                    if (Array.isArray(this.params[key])) {
+                        element.classList.add(...(this.params[key] as string[]))
+                    } else 
+                    if (typeof this.params[key] === 'string') {
+                        element.classList.add(this.params[key] as string)
                     }
                     break
                 default:
-                    (element as any)[key]= this.params[key]
+                    if (typeof this.params[key] === 'string'|| typeof this.params[key] === 'number' || typeof this.params[key] === 'boolean'){
+                        (element[key as keyof HTMLElement] as string) = this.params[key] as keyof object
+                    }
             }
         }
         return element
     }
 }
-
-// interface INodeElement {
-//     textContent?: '',
-//     className: '',
-// }
-
-// interface IImg extends INodeElement {
-//     url: '',
-//     alt: ''
-// }
-
-// interface PropsInput {
-//     type: string
-//     placeholder: string
-// }
-
-// new CreateElement<PropsInput>('asdasd', {
-//     placeholder: 'asdsad',
-// })
-
-
-// type SelectFieldOptionProps {
-//   value?: string
-//   options: string[]
-//   classes: string[]
-// }
-
-// type RadioFieldOptionProps {
-//   value?: string
-//   options: string[]
-//   classes: string[]
-// }
-
-// interface CommonInputProps {
-//   value?: string
-//   classes: string[]
-// }
-
-// interface OptionsRequired {
-//   options: string[]
-// }
-
-// type OptionableFieldProps = CommonInputProps & OptionsRequired
-
-
-// type A = string
-// type B = number 
-// type S = A & B
-
-// interface A extends B {
-
-// }
