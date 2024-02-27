03138dad735958aa655ed400c0c90605b440d3b5
Nataliya Lifer <nathalielifer@gmail.com>
2024-02-27 14:22:37 +0100

    change types and add case classes

--- src/utils/CreateElement/CreateElement.ts
+++ src/utils/CreateElement/CreateElement.ts
@@ -1,21 +1,40 @@
 export default class CreateElement {
     public tagName: string;
-    public params: {[key: string]:any}
-    constructor(tagName: string, params: {[key: string]: any}) {
+    public params: {
+        [key: string]: unknown
+    }
+    constructor(
+        tagName: string,
+        params: {
+            [key: string]: unknown
+        }
+    ) {
         this.tagName = tagName
         this.params = params
     }
     render(): HTMLElement {
-        const element = document.createElement(this.tagName)
+        const element = document.createElement(this.tagName) as HTMLElement
         for (let key in this.params) {
             switch (key) {
                 case 'dataset':
-                    for (let dataKey in this.params[key]) {
-                        element.dataset[dataKey] = this.params[key][dataKey]
+                    if (typeof this.params[key] === 'object') {
+                        for (let dataKey in this.params[key] as object) {
+                            element.dataset[dataKey] = (this.params[key] as keyof object)[dataKey]
+                        } 
+                    }   
+                    break
+                case 'classes': 
+                    if (Array.isArray(this.params[key])) {
+                        element.classList.add(...(this.params[key] as string[]))
+                    } else 
+                    if (typeof this.params[key] === 'string') {
+                        element.classList.add(this.params[key] as string)
                     }
                     break
                 default:
-                    (element as any)[key]= this.params[key]
+                    if (typeof this.params[key] === 'string'|| typeof this.params[key] === 'number' || typeof this.params[key] === 'boolean'){
+                        (element[key as keyof HTMLElement] as string) = this.params[key] as keyof object
+                    }
             }
         }
         return element
